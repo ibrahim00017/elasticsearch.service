c46aff0aa82ffa4fe40258c3b56a3fa00e010e3a
ibrahim00017 <hibrahimyya@gmail.com>
2018-07-20 08:49:48 +0100

    host

--- src/main/java/com/rintio/elastic/client/ElasticClient.java
+++ src/main/java/com/rintio/elastic/client/ElasticClient.java
@@ -34,7 +34,7 @@ public class ElasticClient {
     RestClientBuilder builder;
     RestTemplate restTemplate;
     HttpComponentsClientHttpRequestFactory factory;
-    private String HOST="elasticsearch";
+    private String HOST="5.135.129.180";
     private int port = 9200;
     private int port2 =9201;
 
@@ -84,7 +84,7 @@ public class ElasticClient {
     }
 
     public String construiredockerURI(String index,String type) {
-        return "http://elasticsearch:9200/"+index+"/"+type;
+        return "http://5.135.129.180:9200/"+index+"/"+type;
     }
 
     public SearchCreateOutput creerIndexObjectNative(String index, String type, Object object,String id) throws Exception{
